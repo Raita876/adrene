15a85304f99e4ea1746057a45faa8a52b7f0ce60
Raita Watanabe <watanaberit20171129@gmail.com>
2020-05-22 15:01:32 +0900

    fix TestCreate()

--- image_test.go
+++ image_test.go
@@ -11,13 +11,30 @@ func TestCreate(t *testing.T) {
 			imgPath: "tmp.png",
 			result: Result{
 				Command:  []string{"echo", "Hello World"},
-				Output:   "Hello World",
+				Output:   "Hello World\n",
 				ExitCode: 0,
 			},
 		},
 	}
 
+	im := &ImgMaker{
+		Width:        800,
+		LimitHeight:  2400,
+		MarginTop:    40,
+		MarginLeft:   40,
+		MarginRight:  40,
+		MarginBottom: 0,
+		FontSize:     16,
+		LineSpace:    4,
+		FontType:     "gomonobold",
+		Prompt:       "$",
+	}
+
 	for _, tt := range tests {
+		err := im.Create(tt.imgPath, tt.result)
+		if err != nil {
+			t.Error("Failure ImgMaker.Create()")
+		}
 
 	}
 }
